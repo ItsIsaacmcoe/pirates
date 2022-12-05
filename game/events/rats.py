deac79530399509126f4f15f528249922d8e556a
Isaac Marouk-Coe <imaro001@cslab-130.cs.plattsburgh.edu>
2022-12-05 16:39:56 -0500

    added the basics for a rat event

---
+++ game/events/rats.py
@@ -0,0 +1,7 @@
+#rats will eat food daily until faught
+from game import event
+from game.player import Player
+from game.context import Context
+import game.config as config
+
+Class Rats (context, event.Event):
